2170159801211f1bb9cba1831f01aa96f38afbd6
almeidazs <2hiagoalmeida@gmail.com>
2025-12-16 15:30:18 -0300

    feat(version): adding new command

---
+++ cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/almeidazs/serenity/internal/version"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use: "version",
+	Short: "Show the current version of Serenity",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println(version.Version)
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
---
+++ internal/version/version.go
@@ -0,0 +1,4 @@
+package version
+
+var Version = "dirty"
+var Commit = "unknown"
